a840abc1f2cf5c3beb5c245bee993266e03a6469
riinuots <riinuots@gmail.com>
2023-09-07 11:10:39 +0100

    file rename

--- 01_demographics.R
+++ 01_demographics.R
@@ -3,7 +3,7 @@ library(scales)
 theme_set(theme_bw())
 
 
-demographics_orig = read_csv("/home/common/mover_data/srv/disk00/MOVER/EPIC/EMR/patient_information.csv", 
+demographics_orig = read_csv(paste0(Sys.getenv("epic_emr"), "patient_information.csv"),
                         col_types = cols(HOSP_ADMSN_TIME = col_datetime(format = "%m/%d/%y %H:%M"), 
                                          HOSP_DISCH_TIME = col_datetime(format = "%m/%d/%y %H:%M"), 
                                          SURGERY_DATE = col_datetime(format = "%m/%d/%y %H:%M"), 
@@ -12,13 +12,22 @@ demographics_orig = read_csv("/home/common/mover_data/srv/disk00/MOVER/EPIC/EMR/
                                          AN_START_DATETIME = col_datetime(format = "%m/%d/%y %H:%M"), 
                                          AN_STOP_DATETIME = col_datetime(format = "%m/%d/%y %H:%M")))
 
+
+demographics_orig %>% 
+  count(MRN, LOG_ID, sort = TRUE) %>% 
+  count(LOG_ID, sort = TRUE)
+
+
 demographics = demographics_orig %>% 
   janitor::clean_names() %>% 
   # I've checked that duplicates are of the same admission but one has typos etc
-  distinct(log_id, mrn, .keep_all = TRUE)
+  distinct(log_id, .keep_all = TRUE) %>% 
+  rename(age = birth_date) %>% 
+  mutate(asa_rating = fct_reorder(asa_rating, asa_rating_c))
 
-n_distinct(demographics_orig$MRN)
-n_distinct(demographics_orig$LOG_ID)
+
+# n_distinct(demographics_orig$MRN)
+# n_distinct(demographics_orig$LOG_ID)
 
 # check that no more duplicate procedures
  stopifnot((demographics %>% 
@@ -26,3 +35,8 @@ n_distinct(demographics_orig$LOG_ID)
               pull(n) %>% 
               max()) == 1)
 
+
+ 
+all_procedures = demographics %>% 
+   count(primary_procedure_nm, sort = TRUE)
+ 
--- 03_flowsheets.R
+++ 03_flowsheets.R
@@ -1,36 +1,27 @@
 library(tidyverse)
-library(pointblank)
 
-flowsheet = read_csv("/home/common/mover_data/baldig/bioprojects2/mirana_private/flowsheets_cleaned/flowsheet_part1.csv",
-                     n_max = 20000,
-                     col_types = cols(.default = "c"))
-
-#                     col_types = cols(RECORDED_TIME = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
-
-
-flowsheet2 = read_csv("/home/common/mover_data/baldig/bioprojects2/mirana_private/flowsheets_cleaned/flowsheet_part3.csv",
+flowsheet_orig = read_csv("/home/common/mover_data/baldig/bioprojects2/mirana_private/flowsheets_cleaned/flowsheet_part3.csv",
                      #n_max = 20000,
                      col_types = cols(.default = "c"))
 
-flowsheet2 = janitor::clean_names(flowsheet2) %>% 
-  mutate(recorded_time = ymd_hms(recorded_time))
+flowsheet = janitor::clean_names(flowsheet_orig) %>% 
+  mutate(recorded_time = ymd_hms(recorded_time)) %>% 
+  distinct()
+
+flowsheet %>% 
+  count(record_type, sort = TRUE)
+
+flowsheet %>% 
+  count(record_type, flo_name, sort = TRUE)
+
+flo_display_names = flowsheet %>% 
+  count(flo_display_name, sort = TRUE)
+
+flo_display_names %>%
+  mutate(flo_names = janitor::make_clean_names(flo_display_name))
 
 flowsheet2_min = flowsheet2 %>% 
   group_by(mrn, log_id) %>% 
   summarise(time_min = min(recorded_time), time_max = max(recorded_time))
 
-pointblank_test = flowsheet2 %>% 
-  slice_sample(n = 500000) %>% 
-  scan_data()
 
-flowsheet2 %>% 
-flowsheet2 %>% 
-  mutate(datetime = ymd_hms(RECORDED_TIME), 
-         hour = hour(datetime)) %>% 
-  count(hour)
-
-
-
-flowsheet3 = read_csv("/home/common/mover_data/srv/disk00/EPIC_flowsheets/1_2018-4633_flowsheets_20211006.csv",
-                     n_max = 20000,
-                     col_types = cols(.default = "c"))
