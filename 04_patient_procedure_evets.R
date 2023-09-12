af643db171858d3bbc72f18d049dd1b661557af2
riinuots <riinuots@gmail.com>
2023-09-12 08:59:07 +0100

    date and write added

--- 04_patient_procedure_evets.R
+++ 04_patient_procedure_evets.R
@@ -6,5 +6,15 @@ library(tidyverse)
 
 # Read patient procedure events
 patient_procedure_events <- read_csv("/home/common/mover_data/srv/disk00/MOVER/EPIC/EMR/patient_procedure events.csv", 
-                                     col_types = cols(NOTE_TEXT = col_character())) %>% 
-  janitor::clean_names() 
+                                     col_types = cols(NOTE_TEXT = col_character(),
+                                                      EVENT_TIME = col_datetime(format = "%m/%d/%y %H:%M"))) %>% 
+  janitor::clean_names() %>% 
+  distinct()
+
+
+patient_procedure_events %>% 
+  count(event_display_name, sort = TRUE)
+
+
+write_csv(patient_procedure_events, "/home/common/mover_data/surginf_cleaned/patient_procedure_events_cleaned.csv")
+
